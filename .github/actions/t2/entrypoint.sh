4e637284d14198541740b6d87c042623c9679416
Brayden Ward <ottergaming527@gmail.com>
2026-04-29 13:59:28 -0500

    Enhance entrypoint.sh to handle stage2 deployment
    Add support for stage2 in the entrypoint script with Django secret key.

--- .github/actions/t2/entrypoint.sh
+++ .github/actions/t2/entrypoint.sh
@@ -8,14 +8,13 @@ export ARM_SUBSCRIPTION_ID=${INPUT_ARM_SUBSCRIPTION_ID}
 export ARM_TENANT_ID=${INPUT_ARM_TENANT_ID}
 export STATE_KEY=${INPUT_STATE_KEY}
 export TF_STAGE=${INPUT_TF_STAGE}
-
-az login --service-principal \
-  -u "$ARM_CLIENT_ID" \
-  -p "$ARM_CLIENT_SECRET" \
-  --tenant "$ARM_TENANT_ID"
+export DJANGO_SECRET_KEY_PROD=${INPUT_DJANGO_SECRET_KEY_PROD}
 
 if [[ "$TF_STAGE" == "stage1" ]]; then
   terraform -chdir=${INPUT_TF_STAGE} init -backend-config="key=${INPUT_STATE_KEY}.tfstate"
   terraform -chdir=${INPUT_TF_STAGE} plan -out=${INPUT_TF_STAGE}.tfplan
   terraform -chdir=${INPUT_TF_STAGE} apply ${INPUT_TF_STAGE}.tfplan
+elif [[ "$TF_STAGE" == "stage2" ]]; then
+  terraform -chdir=${INPUT_TF_STAGE} init -backend-config="key=${INPUT_STATE_KEY}.tfstate"
+  terraform -chdir=${INPUT_TF_STAGE} apply -auto-approve -var="ARM_CLIENT_ID=${INPUT_ARM_CLIENT_ID}" -var="ARM_CLIENT_SECRET=${INPUT_ARM_CLIENT_SECRET}" -var="DJANGO_SECRET_KEY=${INPUT_DJANGO_SECRET_KEY_PROD}"
 fi
