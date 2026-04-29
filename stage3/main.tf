b115572e6d02434de67bd158c5908d6e22442a93
Brayden Ward <ottergaming527@gmail.com>
2026-04-29 12:02:19 -0500

    Add Terraform configuration for Azure backend

---
+++ stage3/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.70.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name = "rg-acmp-final"
+    storage_account_name = "acmp2400storageaccount"
+    container_name = "big-tf-state-acmp2400"
+    key = braydenward.tfstate
+    use_azuread_auth = true
+  }
+}
+
+provider "azurerm" {
+  features{}
+}
