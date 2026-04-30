83dba5fd9fb21d4c0eead1153b0add65be6002ed
Brayden Ward <ottergaming527@gmail.com>
2026-04-29 22:00:00 -0500

    Refactor entrypoint.sh for environment variable exports

--- .github/actions/t2/entrypoint.sh
+++ .github/actions/t2/entrypoint.sh
@@ -1,27 +1,18 @@
 #!/bin/bash
-
 set -e
 
-export ARM_CLIENT_ID=${INPUT_ARM_CLIENT_ID}
-export ARM_CLIENT_SECRET=${INPUT_ARM_CLIENT_SECRET}
-export ARM_SUBSCRIPTION_ID=${INPUT_ARM_SUBSCRIPTION_ID}
-export ARM_TENANT_ID=${INPUT_ARM_TENANT_ID}
-export STATE_KEY=${INPUT_STATE_KEY}
-export TF_STAGE=${INPUT_TF_STAGE}
-export DJANGO_SECRET_KEY_PROD=${INPUT_DJANGO_SECRET_KEY_PROD}
+export ARM_CLIENT_ID="${INPUT_ARM_CLIENT_ID}"
+export ARM_CLIENT_SECRET="${INPUT_ARM_CLIENT_SECRET}"
+export ARM_SUBSCRIPTION_ID="${INPUT_ARM_SUBSCRIPTION_ID}"
+export ARM_TENANT_ID="${INPUT_ARM_TENANT_ID}"
+export STATE_KEY="${INPUT_STATE_KEY}"
+export TF_STAGE="${INPUT_TF_STAGE}"
+export DJANGO_SECRET_KEY_PROD="${INPUT_DJANGO_SECRET_KEY_PROD}"
 
-az login --service-principal \
-  -u "$ARM_CLIENT_ID" \
-  -p "$ARM_CLIENT_SECRET" \
-  --tenant "$ARM_TENANT_ID"
+export TF_VAR_ARM_CLIENT_ID="${INPUT_ARM_CLIENT_ID}"
+export TF_VAR_ARM_CLIENT_SECRET="${INPUT_ARM_CLIENT_SECRET}"
+export TF_VAR_DJANGO_SECRET_KEY_PROD="${INPUT_DJANGO_SECRET_KEY_PROD}"
 
-if [[ "$TF_STAGE" == "stage1" ]]; then
-  terraform -chdir=${INPUT_TF_STAGE} init -backend-config="key=${INPUT_STATE_KEY}.tfstate"
-  terraform -chdir=${INPUT_TF_STAGE} plan -out=${INPUT_TF_STAGE}.tfplan
-  terraform -chdir=${INPUT_TF_STAGE} apply ${INPUT_TF_STAGE}.tfplan
-
-elif [[ "$TF_STAGE" == "stage2" ]]; then
-  terraform -chdir=${INPUT_TF_STAGE} init -backend-config="key=${INPUT_STATE_KEY}.tfstate"
-  terraform -chdir=${INPUT_TF_STAGE} import azurerm_container_registry.teacher_acr /subscriptions/${ARM_SUBSCRIPTION_ID}/resourceGroups/rg-braydenward/providers/Microsoft.ContainerRegistry/registries/acrbrayden2400 || true
-  terraform -chdir=${INPUT_TF_STAGE} apply -auto-approve -var="ARM_CLIENT_ID=${INPUT_ARM_CLIENT_ID}" -var="ARM_CLIENT_SECRET=${INPUT_ARM_CLIENT_SECRET}" -var="DJANGO_SECRET_KEY_PROD=${INPUT_DJANGO_SECRET_KEY_PROD}"
-fi
+terraform -chdir="${TF_STAGE}" init -backend-config="key=${STATE_KEY}.tfstate"
+terraform -chdir="${TF_STAGE}" plan -out="${TF_STAGE}.tfplan"
+terraform -chdir="${TF_STAGE}" apply -auto-approve "${TF_STAGE}.tfplan"
